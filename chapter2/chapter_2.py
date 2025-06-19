0a6ea3ffb660e6d09a4f45d0a78b5f9da76c880d
kevinn-28 <kevinn02804@gmail.com>
2025-06-19 14:37:25 +0530

    varialble.keyword,operators

---
+++ chapter2/chapter_2.py
@@ -0,0 +1,44 @@
+#variable,keyword,identifier,constant,comment,string
+a=25 #variable
+b=15
+c="letter A"
+print(a+b) 
+print(c)
+d=None #None is a constant
+print(d)
+_max=100 #identifier
+print(_max)
+print(a,b,c,d,_max) 
+
+print("arithmetic operators")
+#arithmetic operators
+a=10
+print(a+5) 
+b=25
+print(a+b)
+print(a ,b)
+
+print("assignment operator ")
+d=10
+print(d)
+d += 5
+print(d)
+
+
+#comparison operators
+print("comparison operators")
+e=6
+f=9
+print(e==f) 
+print(e>f)
+print(e<f)
+print(e!=f)
+
+
+#logical operators
+print("logical operators")
+e = 6
+f = 9
+print(e==f and e>f)  
+print(e==f or e<f)
+print(not(e>f))
