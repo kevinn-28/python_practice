f3a654e9d0261c6b412f4f51db991f17787af2c0
kevinn-28 <kevinn02804@gmail.com>
2025-06-19 11:16:33 +0530

    Add files via upload

---
+++ chapter1_ii.py
@@ -0,0 +1,5 @@
+import time
+time.sleep(5)
+print("this message will be printed after 5 seconds")
+#this is a inbuilt-in module
+
---
+++ chapter1_iii.py
@@ -0,0 +1,5 @@
+#this is a third-party module install using pip
+import pyttsx3
+engine =pyttsx3.init()
+engine.say("hello sir how can i help you")
+engine.runAndWait()
