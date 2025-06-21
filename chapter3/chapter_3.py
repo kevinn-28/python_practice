b01b01c2e5811d6f09b42855aae4fa0417c3cd63
kevinn-28 <kevinn02804@gmail.com>
2025-06-21 13:47:25 +0530

    strings

---
+++ chapter3/chapter_3.py
@@ -0,0 +1,10 @@
+#strings
+Name="tim'"
+Name2='wick"'
+Name3="""o'k , we can single ' or double " quotes"""
+print(Name)
+print(Name2)
+print(Name3)
+print(type(Name))
+print(type(Name2)) 
+print(type(Name3))
