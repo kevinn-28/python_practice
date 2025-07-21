d03ae400959767d50f9afaf7620ad28f1430cd35
kevinn-28 <kevinn02804@gmail.com>
2025-07-21 13:57:34 +0530

    function

---
+++ chatpter8/chapter8.py
@@ -0,0 +1,11 @@
+def func1():
+    print("hello ")
+    print("welcome to function")
+    print("additional line")
+
+func1()
+func1()
+
+def pp():
+    print("\nwelcome")
+pp()
---
+++ chatpter8/chapter_8_i.py
@@ -0,0 +1,11 @@
+def average(a,b,c):
+    return(a+b+c)/3
+avg=average(3,6,3)
+print(avg)
+
+
+def greet(name="kevin"):
+    a="hello ,"+name
+    return a
+l=greet(" welcome to return function")
+print(l)
